15a25824008c21b72a1c681c5cb7da491f4ea519
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-06 14:21:24 -0400

    Default Property File - "Application.properties"

--- restEndPointGet/src/main/java/frank/in/restEndPointGet/Controller/REstEndPoint.java
+++ restEndPointGet/src/main/java/frank/in/restEndPointGet/Controller/REstEndPoint.java
@@ -1,14 +1,42 @@
 package frank.in.restEndPointGet.Controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class REstEndPoint {
 
-    public Course getEndPoint(){
-        return Course(name,chapterCount);
+    @Value("${default.course.name}")
+    private String cName;
+
+    @Value("${default.course.chapterCount}")
+    private int chaptersCount;
+
+
+
+
+    @RequestMapping("/defaultCourse")
+    public Course getDefaultCourse(@RequestParam(value = "name",required = false)String name,
+                              @RequestParam(value = "chapterCount",required = false)int chapterCount
+    ){
+        return new Course(cName,chaptersCount);
     }
 
+
+    @RequestMapping("/course")
+    public Course getEndPoint(@RequestParam(value = "name", defaultValue = "Spring Boot",required = false)String name,
+                              @RequestParam(value = "chapterCount", defaultValue = "2",required = false)int chapterCount
+                              ){
+        return new Course(name,chapterCount);
+    }
+
+
+
+@RequestMapping(method = RequestMethod.POST, value = "/register/course")
+    public String saveCourse(@RequestBody Course course){
+
+    return "Your course named " + course.getName() + " with " + course.getChapterCount() + " chapters saved";
+    }
 
 }
