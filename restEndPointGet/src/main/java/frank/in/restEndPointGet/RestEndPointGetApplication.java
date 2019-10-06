e4f7e0137a496d9c3c4407fd9502daeff4358bfe
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-06 12:10:26 -0400

    Starter Code

---
+++ restEndPointGet/src/main/java/frank/in/restEndPointGet/RestEndPointGetApplication.java
@@ -0,0 +1,13 @@
+package frank.in.restEndPointGet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndPointGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEndPointGetApplication.class, args);
+	}
+
+}
